e0a4d76779cf041c0f0eb29ebe1a01f32392c4a8
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-21 10:59:49 -0600

    Further path fixes

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
+REGISTRY_PATHS="$(sbcl --noinform --load dev/list-dirs.lisp --non-interactive)"
 Xvfb :1 &
-sbcl --noinform \
+CL_SOURCE_REGISTRY="$REGISTRY_PATHS" sbcl --noinform \
   --non-interactive \
   --load "$PWD"/ci/system.lisp \
   --eval "(in-package #:replwm-tests)" \
