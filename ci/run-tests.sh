eda206c78b4e5eba11e182c4a233e7d2211e7bc0
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-21 11:18:46 -0600

    Further modifications; trying to find failure

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -4,7 +4,5 @@ REGISTRY_PATHS="$(sbcl --noinform --load dev/list-dirs.lisp --non-interactive)"
 Xvfb :1 &
 CL_SOURCE_REGISTRY="$REGISTRY_PATHS" sbcl --noinform \
   --non-interactive \
-  --load "$PWD"/ci/system.lisp \
-  --eval "(in-package #:replwm-tests)" \
-  --eval "(x11-suite)"
+  --load "$PWD"/ci/system.lisp
 pkill Xvfb;
