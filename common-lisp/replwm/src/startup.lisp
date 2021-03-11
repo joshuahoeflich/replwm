71661bcccd3404d25a7f87ed8d1e584d8339c7b8
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-03-11 15:20:53 -0600

    Refactor to use common lisp idioms

--- common-lisp/replwm/src/startup.lisp
+++ common-lisp/replwm/src/startup.lisp
@@ -1,40 +1,38 @@
 (in-package #:replwm)
 
-(defun log-fatal-error! (err)
-  (format *error-output* "Fatal error: ~A~%Exiting replwm." err))
+(defstruct wm-state display screen root)
 
-(defun open-x11! ()
+(defun create-wm-state! ()
   (let* ((display (xlib:open-default-display))
          (screen (xlib:display-default-screen display))
          (root (xlib:screen-root screen)))
-    (values display screen root)))
+    (make-wm-state
+     :display display
+     :screen screen
+     :root root)))
 
-(defun check-other-wm! (display root)
-  (setf (xlib:window-event-mask root)
-        '(:substructure-notify :substructure-redirect))
-  (xlib:rr-select-input
-   root
-   '(:screen-change-notify-mask :crtc-change-notify-mask))
-  (xlib:display-finish-output display))
+(defmethod check-other-wm! ((state wm-state))
+  (let ((display (wm-state-display state))
+        (root (wm-state-root state)))
+    (setf (xlib:window-event-mask root)
+          '(:substructure-notify :substructure-redirect))
+    (xlib:rr-select-input
+     root
+     '(:screen-change-notify-mask :crtc-change-notify-mask))
+    (xlib:display-finish-output display)
+    state))
 
-(defun setup! ()
-  (handler-case
-      (multiple-value-bind (display screen root) (open-x11!)
-        (check-other-wm! display root)
-        (values display screen root))
-    (t (err) (log-fatal-error! err))))
+(defun setup-replwm! ()
+  (handler-case (check-other-wm! (create-wm-state!))
+    (sb-bsd-sockets:socket-error ()
+      (format *error-output* "Could not connect to X11.~%"))
+    (xlib:access-error ()
+      (format *error-output* "Another window manager is running.~%"))))
 
-(defun handle-x11-event! (display screen root)
-  (declare (ignore display screen root))
-  :quit)
+;; (defun handle-x11-event! (display screen root)
+;;   (declare (ignore display screen root))
+;;   :quit)
 
-(defun event-loop (display screen root)
-  (loop :for event = (handle-x11-event! display screen root)
-        :until (eq event :quit)))
-
-(defun start-wm! ()
-  (multiple-value-bind (display screen root) (setup!)
-    (when display
-      (unwind-protect
-           (event-loop display screen root)
-        (xlib:close-display display)))))
+;; (defun event-loop (display screen root)
+;;   (loop :for event = (handle-x11-event! display screen root)
+;;         :until (eq event :quit)))
--- common-lisp/replwm/test/startup.lisp
+++ common-lisp/replwm/test/startup.lisp
@@ -1,25 +1,24 @@
 (in-package #:replwm-tests)
 
-;; Because running this suite requires the presense of another window manager,
-;; we don't yet execute it in our CI pipeline. Pull requests welcome for that
-;; use case!
+(defun stringify-stderr (fn)
+  (let ((*error-output* (make-string-output-stream)))
+    (funcall fn)
+    (get-output-stream-string *error-output*)))
+
+;; Because running this suite requires another window manager to exist on
+;; DISPLAY :0, we don't yet execute it in our CI pipeline. Pull requests
+;; welcome!
 (defsuite startup-error-suite
   (test
-   (search
-    "ACCESS-ERROR"
-    (let ((*error-output* (make-string-output-stream)))
-      (setup!)
-      (get-output-stream-string *error-output*))))
-  (test
-   (search
-    "ACCESS-ERROR"
-    (let ((*error-output* (make-string-output-stream)))
-      (start-wm!)
-      (get-output-stream-string *error-output*))))
+   (string= (format nil "Another window manager is running.~%")
+            (stringify-stderr #'setup-replwm!)))
   (sb-posix:setenv "DISPLAY" ":2" 1)
-  (test (search
-         "Socket error"
-         (let ((*error-output* (make-string-output-stream)))
-           (setup!)
-           (get-output-stream-string *error-output*))))
+  (test
+   (string= (format nil "Could not connect to X11.~%")
+            (stringify-stderr #'setup-replwm!)))
+  (sb-posix:setenv "DISPLAY" ":0" 1))
+
+(defsuite startup-success-suite
+  (sb-posix:setenv "DISPLAY" ":1" 1)
+  (test (wm-state-p (setup-replwm!)))
   (sb-posix:setenv "DISPLAY" ":0" 1))
