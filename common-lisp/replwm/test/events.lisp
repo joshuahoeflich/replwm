604446ffd44793003880649d9324e1ee78ccc103
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-03-12 10:00:44 -0600

    Refactors test code

--- common-lisp/replwm/test/events.lisp
+++ common-lisp/replwm/test/events.lisp
@@ -13,17 +13,15 @@
 (defsuite event-loop-suite
   (test (string=
          (format nil "We exited the wm with display NIL~%")
-         (let ((*standard-output* (make-string-output-stream)))
+         (stringify-stream *standard-output*
            (event-loop (make-wm-state :display nil :screen nil :root nil
                                       :on-event #'send-quit-mock
-                                      :on-exit #'on-exit-mock))
-           (get-output-stream-string *standard-output*))))
+                                      :on-exit #'on-exit-mock)))))
   (test (string=
          (format nil "We exited the wm with display NIL~%An error ran!")
-         (let ((*standard-output* (make-string-output-stream)))
+         (stringify-stream *standard-output*
            (handler-case
                (event-loop (make-wm-state :display nil :screen nil :root nil
                                           :on-event #'error-mock
                                           :on-exit #'on-exit-mock))
-             (t () (format t "An error ran!")))
-           (get-output-stream-string *standard-output*)))))
+             (t () (format t "An error ran!")))))))
--- common-lisp/replwm/test/startup.lisp
+++ common-lisp/replwm/test/startup.lisp
@@ -1,27 +1,27 @@
 (in-package #:replwm-tests)
 
-(defmacro stringify-stderr (&body code)
-  `(let ((*error-output* (make-string-output-stream)))
+(defmacro stringify-stream (stream &body code)
+  `(let ((,stream (make-string-output-stream)))
      ,@code
-     (get-output-stream-string *error-output*)))
+     (get-output-stream-string ,stream)))
 
 (defsuite startup-error-suite
   (test
    (string= (format nil "Could not connect to X11.~%")
-            (stringify-stderr
+            (stringify-stream *error-output*
               (catch-startup-errors
                (lambda () (sb-bsd-sockets:socket-error "Socket connection error."))))))
   (test
    (string= (format nil "Another window manager is running.~%")
-            (stringify-stderr
+            (stringify-stream *error-output*
               (catch-startup-errors
                (lambda () (signal 'xlib:access-error))))))
   (test (string= (format nil "Unexpected error: Success.~%")
-                 (stringify-stderr
+                 (stringify-stream *error-output*
                    (catch-startup-errors
                     (lambda () (error "Success."))))))
   (test (string= (format nil "Unexpected error: Args passed.~%")
-                 (stringify-stderr
+                 (stringify-stream *error-output*
                    (catch-startup-errors
                     (lambda (arg) (error (format nil "Args ~A." arg)))
                     "passed")))))
